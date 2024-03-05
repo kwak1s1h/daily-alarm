4489db496a8e66bf6b018a7e644dd13c959e7309
kwak1s1h <kwak1s1h@kakao.com>
2024-03-05 16:22:28 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+const axios = require('axios').default;
+const cheerio = require("cheerio");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const host = 'https://ggm.gondr.net';
+
+const webhookClient = new WebhookClient({ id: process.env.WEBHOOK_ID, token: process.env.WEBHOOK_TOKEN });
+const now = new Date(Date.now());
+
+getDailyNotes().then(list => {
+	const embed = new EmbedBuilder()
+		.setTitle('**일간보고서를 작성해주세요 (클릭 시 이동)**')
+		.setURL(`${host}/project/team/27`)
+		.setFields(list)
+		.setColor(0x00FFFF)
+		.setTimestamp()
+		.setFooter({text: `${list.length} / 5`});
+	
+	if(list.length <= 0)
+	{
+		embed.setDescription('아무도 작성하지 않음.')
+	}
+	
+	webhookClient.send({
+		content: '@everyone',
+		username: '일간보고서 작성 알림',
+		embeds: [embed],
+	});
+})
+
+async function getDailyNotes() {
+	let page = await getPage();
+	let list = page.data.list;
+    return list.map(elem => {
+		return { name: elem.name, value: elem.content };
+	});
+}
+
+function getDateString(date) {
+	return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일 ${date.getHours()}시 ${date.getMinutes()}분`;
+}
+
+async function getPage() {
+    return await axios.get(`${host}/api/team/record/daily?team=27&start_day=${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`, {
+        headers: { "Authorization" : `Bearer ${process.env.TOKEN}` }
+    });
+}
